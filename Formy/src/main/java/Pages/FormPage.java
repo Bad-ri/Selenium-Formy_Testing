e123ec0f666748914e33fdd658fd8282eab8f983
Badri20194841 <20194841@FUE.EDU.EG>
2023-03-18 07:06:44 +0200

    Apply TestNG framework

--- Formy/src/main/java/Pages/FormPage.java
+++ Formy/src/main/java/Pages/FormPage.java
@@ -1,6 +1,7 @@
 package Pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class FormPage{
     private WebDriver driver;
@@ -9,13 +10,28 @@ public class FormPage{
     }
     By first_name = By.cssSelector("input[id=\"first-name\"]");
     By last_name = By.cssSelector("input[id=\"last-name\"]");
+    By job_title = By.cssSelector("input[id=\"job-title\"]");
+    By education_level = By.cssSelector("input[id=\"radio-button-1\"]");
+    By sex = By.cssSelector("input[id=\"checkbox-2\"]");
+    By experience_year = By.cssSelector("select[id=\"select-menu\"]");
 
     public void EnterFirstName(String FirstName){
     driver.findElement(first_name).click();
     driver.findElement(first_name).sendKeys(FirstName);
 
 }
-    public void EnterLastName(String LastName){
-        driver.findElement(last_name).sendKeys(LastName);
+    public void EnterLastName(String LastName){driver.findElement(last_name).sendKeys(LastName);
+    }
+    public void EnterJobTitle(String JobTitle){
+        driver.findElement(job_title).click();
+        driver.findElement(job_title).sendKeys(JobTitle);
+    }
+    public void EnterEducationLevel(){driver.findElement(education_level).click();
+    }
+    public void EnterSex(){driver.findElement(sex).click();
+    }
+    public void EnterExperienceYear(int Experience){
+        Select year = new Select(driver.findElement(experience_year));
+        year.selectByIndex(Experience);
     }
 }
--- Formy/src/test/java/TestPage/SignUp.java
+++ Formy/src/test/java/TestPage/SignUp.java
@@ -9,11 +9,15 @@ public class SignUp extends Base{
     public void SignUp() throws InterruptedException {
        FormPage FormObject = new FormPage(driver);
        HomePage HomeObject = new HomePage(driver);
-        Thread.sleep(2000);
+       Thread.sleep(2000);
        HomeObject.OpenSignUp();
        Thread.sleep(2000);
        FormObject.EnterFirstName("Mohamed");
        FormObject.EnterLastName("Elbadri");
+       FormObject.EnterJobTitle("student");
+       FormObject.EnterEducationLevel();
+       FormObject.EnterSex();
+       FormObject.EnterExperienceYear(2);
     }
 
 }
